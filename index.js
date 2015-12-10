7a413f5ebee2871bcd201c8d95969021fc4c7691
artofspeed <joesmithz2008@gmail.com>
2015-12-09 22:49:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -198,33 +198,33 @@ function adapter(uri, opts){
   };
 
 
-  // Set up exit handlers so we can clean up this process's redis data before exiting
+  // // Set up exit handlers so we can clean up this process's redis data before exiting
 
-  process.stdin.resume(); //so the program will not close instantly
-  function exitHandler(options, err){
-    var i;
-    var multi = data.multi();
-    var execDone = false;
+  // process.stdin.resume(); //so the program will not close instantly
+  // function exitHandler(options, err){
+  //   var i;
+  //   var multi = data.multi();
+  //   var execDone = false;
 
-    var roomIds = Object.keys(self.rooms);
-    var socketIds = Object.keys(self.sids);
-    for(i=0; i<roomIds.length; ++i){
-      multi.srem(prefix + '#' + roomIds[i], Object.keys(self.rooms[roomIds[i]]));
-    }
-    for(i=0; i<socketIds.length; ++i){
-      multi.srem(prefix + '#' + socketIds[i], Object.keys(self.sids[socketIds[i]]));
-    }
-    multi.exec(function(err, replies){
-      process.exit();
-    });
-  }
+  //   var roomIds = Object.keys(self.rooms);
+  //   var socketIds = Object.keys(self.sids);
+  //   for(i=0; i<roomIds.length; ++i){
+  //     multi.srem(prefix + '#' + roomIds[i], Object.keys(self.rooms[roomIds[i]]));
+  //   }
+  //   for(i=0; i<socketIds.length; ++i){
+  //     multi.srem(prefix + '#' + socketIds[i], Object.keys(self.sids[socketIds[i]]));
+  //   }
+  //   multi.exec(function(err, replies){
+  //     process.exit();
+  //   });
+  // }
  
-  // //do something when app is closing
-  // process.on('exit', exitHandler.bind(null,{cleanup:true}));
-  process.on('SIGTERM', exitHandler);
-  process.on('SIGINT', exitHandler);
-  process.on('SIGQUIT', exitHandler);
-  process.on('uncaughtException', exitHandler);
+  // // //do something when app is closing
+  // // process.on('exit', exitHandler.bind(null,{cleanup:true}));
+  // process.on('SIGTERM', exitHandler);
+  // process.on('SIGINT', exitHandler);
+  // process.on('SIGQUIT', exitHandler);
+  // process.on('uncaughtException', exitHandler);
  
 
   return Redis;
